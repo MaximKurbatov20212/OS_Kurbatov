2e6517b7b87796defbef59810d741d153bab0be3
Kurbatov2012 <m.kurbatov@g.nsu.ru>
2022-02-13 21:34:13 +0200

    added second solution

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -2,28 +2,26 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
-extern char *tzname[];
+#define OFFSET -15
 
 int main() {
     time_t now;
-    struct tm *sp;
+    struct tm* sp;
 
-    (void)time(&now);
-
+    time(&now);
+    now += OFFSET * 60 * 60;
     sp = localtime(&now);
+    
+    // Another solution 
+    // sp->tm_hour += OFFSET; 
+    // time_t california_time = mktime(sp); // struct -> time_t
+    // sp = localtime(&california_time);    // time_t -> struct
+
+    printf("Time in California: ");
     printf("%d/%d/%02d %d:%02d %s\n",
            sp->tm_mon + 1, sp->tm_mday,
            sp->tm_year, sp->tm_hour,
            sp->tm_min, tzname[sp->tm_isdst]);
 
-    sp->tm_hour -= 15; // difference between NSK and CA = 15
-    time_t california_time = mktime(sp);
-    
-    sp = localtime(&california_time);
-   
-    printf("%d/%d/%02d %d:%02d %s\n",
-           sp->tm_mon + 1, sp->tm_mday,
-           sp->tm_year, sp->tm_hour,
-           sp->tm_min, tzname[sp->tm_isdst]);
     return 0;
 } 
