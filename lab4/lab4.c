85be403f362ee5c9c4a9e65c87210f7bab5b1428
Kurbatov2012 <m.kurbatov@g.nsu.ru>
2022-02-28 00:05:51 +0200

    Fixed allocations of memory

--- lab4/lab4.c
+++ lab4/lab4.c
@@ -1,22 +1,38 @@
 #include "list.h"
-#define MAX_BUF 80 
 
 char* scan_info() {
-    char* data = malloc(MAX_BUF * sizeof(char));
-    fgets(data, MAX_BUF, stdin);
-    data[strlen(data) - 1] = 0;
+    char* data = NULL;
+    size_t len = 0;
+
+    ssize_t l = getline(&data, &len, stdin);
+    if(l == -1) {
+        fprintf(stderr, "Couldn't allocate memory");
+        return NULL;   
+    }
+
     return data;
 }
 
 int main() {
-    printf("Type info until . : \n");
     Node* list = NULL;
+    int err = SUCCESS;
     char* data = scan_info();
 
-    while(data[0] != '.') {
-        list = insert(list, data);
-        data = scan_info();
+    if(data == NULL) {
+        free_list(list);
+        return 0;
     }
+
+    while(data[0] != '.') {
+        list = insert(list, data, &err);
+
+        if(err == ERROR) break;
+
+        data = scan_info();
+
+        if(data == NULL) break;
+    }
+
     print_list(list);
     free_list(list);
     return 0;
