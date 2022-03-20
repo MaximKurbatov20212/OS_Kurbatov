4bae5ae2694c8bf24c818041dc23ce7b0e21ede0
Kurbatov2012 <m.kurbatov@g.nsu.ru>
2022-03-20 20:46:57 +0200

    Added description, fixed putenv()

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -2,26 +2,50 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
-#define OFFSET -15
+
+#define SUCCESS 0
+#define PUTENV_ENOMEM 1
+#define TIME_ERROR (time_t)-1
+#define CTIME_ERROR 2
 
 int main() {
     time_t now;
-    struct tm* sp;
 
-    time(&now);
-    now += OFFSET * 60 * 60;
-    sp = localtime(&now);
+    // int putenv(char* string) - изменяет или добавляет значение переменной среды. string должна имет вид NAME=VALUE. Также, если просто
+    // вызвать putenv(NAME), то произойдет удаление NAME из среды.
+    // В случае успеха возвращет 0, иначе не 0.
+
+    // Попытка изменить переменную среды TZ.
+    int putenv_result = putenv("TZ=America/Los_Angeles");
+    if(putenv_result != SUCCESS) {
+        perror("Couldn't change TZ");
+        return PUTENV_ENOMEM;
+    }
     
-    // Another solution 
-    // sp->tm_hour += OFFSET; 
-    // time_t california_time = mktime(sp); // struct -> time_t
-    // sp = localtime(&california_time);    // time_t -> struct
+    
+    // time возвращает количество секунд, прошедших с 00:00:00 1 января 1970 года времени UTC
+    // -1 в случае неудачи
 
+    // Пытаемся получить время в секундах
+    time_t time_result = time(&now);
+    if(time_result == TIME_ERROR) {
+        perror("Couldn't get time");
+        return TIME_ERROR;
+    }
+    
+
+    // char* ctime(time_t) в качестве агрумента используют тип данных time_t , представляющий собой календарное время.
+    // Функция интерпетирует количество секунд, прошедших с 00:00:00 1 января 1970 года времени UTC, как строку и возвращет ее.
+
+    // Перевод секунд в строку, содержащую текущую дату
+    char* string_date = ctime(&now);
+    if (string_date == NULL) {
+        perror("Couldn't get time as string");
+        return CTIME_ERROR;
+    }
+
+    // Печать времени
     printf("Time in California: ");
-    printf("%d/%d/%02d %d:%02d %s\n",
-           sp->tm_mon + 1, sp->tm_mday,
-           sp->tm_year, sp->tm_hour,
-           sp->tm_min, tzname[sp->tm_isdst]);
-
-    return 0;
+    printf("%s \n", string_date);
+    return SUCCESS;
 } 
