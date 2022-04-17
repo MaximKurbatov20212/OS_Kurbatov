4fac48528eeb05b4ea21589b3959c3ed2a09acaf
Kurbatov2012 <m.kurbatov@g.nsu.ru>
2022-04-17 15:25:05 +0200

    Fixed execvp, handled errors

--- lab9/lab9_2.c
+++ lab9/lab9_2.c
@@ -8,25 +8,26 @@
 #define SUBPROCESS_PID 0
 #define INIT_SUBPROCESS_ERROR -1
 #define WAIT_ERROR -1
+#define EXECVP_ERROR 2
 
-int main() {
+int main(int argc, char** argv) {
 
-    // Создаем дочерний процесс
     pid_t fork_result = fork();
     if (fork_result == INIT_SUBPROCESS_ERROR) {
         perror("Couldn't init subprocess");
         return INIT_SUBPROCESS_ERROR;
     }
 
-    // Условие на PID, что это дочерний процесс
     if (fork_result == SUBPROCESS_PID) {
-        char *parametrs[] = {"cat", "large.txt", NULL};
-        // вызов: cat large.txt. Те мы подменяем текущую программу lab9.exe на cat.exe.
-        execvp(parametrs[0], parametrs);
-        return SUCCESS;
+        // Тут переписал, чтобы мы могли подтягивать имя файла, как аргумент из командной строки.
+        execvp("cat", argv);
+
+        // Исправил тут. Если execvp завершится удачно, то не произойдет обратная передача управления.
+        // Поэтому, если мы попали на данную строчку, значит execvp завешился с ошибкой и вернул управление.
+        perror("Execvp error");
+        return EXECVP_ERROR;
     }
 
-    // Ждем, пока дочерний процесс не завершится
     pid_t wait_result = wait(NULL);
     if(wait_result == WAIT_ERROR) {
         perror("Couldn't wait");
